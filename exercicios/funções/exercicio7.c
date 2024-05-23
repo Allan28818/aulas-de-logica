90f7cf7544d3183f9c7c81b637e7eb170c97c4b8
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2024-05-23 09:20:41 -0300

    chore(comment): comment added to exercise 7

--- exercicios/funções/exercicio7.c
+++ exercicios/funções/exercicio7.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 7. **Simular um dado de 6 faces:**
+// Crie uma função chamada `Dado()` que
+// retorne, através de sorteio, um número de
+// 1 até 6.
 int simulateDice() {
   srand(time(NULL));
   return (rand() % 6) + 1;
